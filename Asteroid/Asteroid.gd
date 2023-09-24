f4295858634d8f3ef69db99f7a144c8e21146013
itzjadenh <142930072+itzjadenh@users.noreply.github.com>
2023-09-24 19:49:33 -0400

    completes assingment 3

--- Asteroid/Asteroid.gd
+++ Asteroid/Asteroid.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 var initial_speed = 100.0
-
+var health = 10
 func _ready(): 
 	velocity = Vector2(0,initial_speed*randf()).rotated(2*PI*randf())
 	
@@ -12,3 +12,7 @@ func _physics_process(_delta):
 	position.y = wrapf(position.y,0,648)
 	
 
+func damage(d):
+	health -= d
+	if health <= 0:
+		queue_free()
